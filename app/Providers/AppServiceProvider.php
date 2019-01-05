626263032f9526e7fed97ff91e7e95a865ac810e
HP-Pavilion <abasheerbarakat@gmail.com>
2019-01-05 23:09:02 +0200

    fix https issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 
     /**
@@ -23,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if($this->app->environment('production')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
