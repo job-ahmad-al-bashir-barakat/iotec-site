ec128cf3b0b09e0ac54fa901f2ee2cdda56eb7bb
HP-Pavilion <abasheerbarakat@gmail.com>
2019-01-05 19:32:21 +0200

    change composer

--- nova-components/Autocomplete/resources/js/field.js
+++
@@ -1,5 +0,0 @@
-Nova.booting((Vue, router) => {
-    Vue.component('index-autocomplete', require('./components/IndexField'));
-    Vue.component('detail-autocomplete', require('./components/DetailField'));
-    Vue.component('form-autocomplete', require('./components/FormField'));
-})
--- nova-components/Croppie/resources/js/field.js
+++
@@ -1,5 +0,0 @@
-Nova.booting((Vue, router) => {
-    Vue.component('index-croppie', require('./components/IndexField'));
-    Vue.component('detail-croppie', require('./components/DetailField'));
-    Vue.component('form-croppie', require('./components/FormField'));
-})
