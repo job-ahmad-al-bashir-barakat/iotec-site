b6850dc543fd80c16df1ba52f718709fdb16f9a9
HP-Pavilion <abasheerbarakat@gmail.com>
2019-01-22 17:03:37 +0200

    change composer

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,7 +51,7 @@ return [
         'public' => [
             'driver' => 'local',
             // 'root' => storage_path('app/public'),
-            'root' => public_path(),
+            'root' => public_path('/storage'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
