91bc4b2d4f94273f460b99dc7eec6c50d8889df1
Ahmad Al Bashir Barakat <53353977+job-ahmad-al-bashir-barakat@users.noreply.github.com>
2020-01-08 03:51:47 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
     {
         if($this->app->environment('production')) {
             // force https
-            // \URL::forceScheme('https');
+            \URL::forceScheme('https');
 
             // fix Specified key was too long; max key length is 767 bytes error
             \Schema::defaultStringLength(191);
@@ -38,7 +38,7 @@ class AppServiceProvider extends ServiceProvider
         if($this->app->environment('production')) {
 
             // force https
-            // $this->app['request']->server->set('HTTPS', true);
+            $this->app['request']->server->set('HTTPS', true);
 
             // set db config
             $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
